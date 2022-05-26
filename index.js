eef91bba3e0d778e808c5f840430b038ae93ec85
Kyle Tillson <kyletillson004@gmail.com>
2022-05-26 13:17:50 -0600

    commit tree 5096031ebddff0b38d5bbeba65242f378587a312

---
+++ index.js
@@ -0,0 +1,13 @@
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+  function introduction(name) {
+     return (`Hi, my name is ${name}.`);
+    
+  }
+  function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
